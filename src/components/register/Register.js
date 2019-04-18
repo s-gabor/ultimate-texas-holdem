407325eb424ddab4cd7b3ba15bbd9033224f1b3c
s-gabor <ing.gabor@yahoo.com>
2019-04-18 21:39:42 +0300

    add heroku url

--- src/components/register/Register.js
+++ src/components/register/Register.js
@@ -30,7 +30,7 @@ class Register extends React.Component {
     onSubmitRegister = (event) => {
         event.preventDefault();
 
-        fetch('http://localhost:3005/register', {
+        fetch('https://ultimate-texas-holdem-api.herokuapp.com/register', {
                 method: 'post',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
--- src/components/signin/Signin.js
+++ src/components/signin/Signin.js
@@ -25,7 +25,7 @@ class Signin extends React.Component {
     onSubmitSignIn = (event) => {
         event.preventDefault();
 
-        fetch('http://localhost:3005/signin', {
+        fetch('https://ultimate-texas-holdem-api.herokuapp.com/signin', {
                 method: 'post',
                 headers: {'Content-Type': 'application/json'},
                 body: JSON.stringify({
